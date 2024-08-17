4b5f5dc6ad72911d1ed90b96e3dbeacac3468d59
IvanB <va.baklanov@mail.ru>
2024-08-18 00:58:31 +0500

    included auth router from fastapi users

--- src/api/api_v1/auth.py
+++ src/api/api_v1/auth.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
+from .fastapi_users import fastapi_users
+from src.api.dependencies.authentication.backend import authentication_backend
 from core.config import settings
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
 )
